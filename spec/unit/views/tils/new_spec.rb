c14cc564b04335f4c57ef18a2eca6d4e34933405
GustavoCaso <gustavocaso@gmail.com>
2017-07-28 08:50:02 +0200

    Added show til view

---
+++ spec/unit/views/tils/new_spec.rb
@@ -0,0 +1,37 @@
+require "db_spec_helper"
+require "til_web/views/tils/new"
+require "til_web/view/context"
+
+RSpec.describe TilWeb::Views::Tils::New, "#call", type: :view do
+  subject(:view) { described_class.new }
+
+  let(:context) {
+    TilWeb::View::Context.new(
+      fullpath: "/",
+      csrf_metatag: -> { "" },
+      csrf_tag: -> { "" },
+      csrf_token: -> { "" },
+      flash: {},
+    )
+  }
+
+  let(:html) { view.(context: context, **dependecies) }
+  let(:page) { Capybara.string(html) }
+
+
+  context 'without errors' do
+    let(:dependecies) { {} }
+    it "renders the til" do
+      expect(page).to_not have_selector('.error')
+      expect(page).to have_selector "form"
+    end
+  end
+
+  context 'with errors' do
+    let(:dependecies) { {errors: ['I fail at university']} }
+    it "renders the til" do
+      expect(page).to have_selector('.error', text: 'I fail at university')
+      expect(page).to have_selector "form"
+    end
+  end
+end
